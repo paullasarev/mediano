1b20d8570a68c3079716da6607cacc21f25b465c
paullasarev <paullasarev@gmail.com>
2014-04-19 17:01:21 +0400

    fix comments

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,4 +1,4 @@
-// app.directive('myText', ['$rootScope', function($rootScope) {
+
 module.exports.ContentText = function($rootScope) {
   return {
     link: function(scope, element, attrs) {
@@ -7,13 +7,6 @@ module.exports.ContentText = function($rootScope) {
         //console.log('scope: ' + scope);
         var val = theVal;
 
-        // if (document.selection) {
-        //   domElement.focus();
-        //   var sel = document.selection.createRange();
-        //   sel.text = val;
-        //   domElement.focus();
-        // } else 
-        // if (domElement.selectionStart || domElement.selectionStart === 0) {
           var startPos = domElement.selectionStart;
           var endPos = domElement.selectionEnd;
           if (startPos > domElement.value.length) {
@@ -61,10 +54,7 @@ module.exports.ContentText = function($rootScope) {
           domElement.selectionStart = startPos + valLen;
           domElement.selectionEnd = startPos + valLen;
           domElement.scrollTop = scrollTop;
-        // } else {
-        //   domElement.value += val;
-        //   domElement.focus();
-        // }
+
         scope.content = domElement.value;
         scope.Changed();
       });
